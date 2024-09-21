cf9f4b52d77401f62b7a3e03caf2205add98dc75
Marco Haber <marcoshaber99@gmail.com>
2024-09-21 13:14:40 +0300

    reduce intensity of bg grid effect on light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {/* Background grid and gradient */}
-          <div className="fixed inset-0 bg-grid-small-black/[0.2] dark:bg-grid-small-white/[0.1] -z-10" />
+          <div className="fixed inset-0 bg-grid-small-black/[0.1] dark:bg-grid-small-white/[0.1] -z-10" />
           <div className="fixed inset-0 bg-white dark:bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] -z-10" />
 
           {/* Main content */}
