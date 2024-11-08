9a1618ef6d99bcb942765db9bbbbf833e4924ac2
Marco Haber <marcoshaber99@gmail.com>
2024-11-08 13:58:49 +0200

    all photographs are now stored on uploadthing

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,19 @@ import rehypePrism from "rehype-prism-plus";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/a/lllwu0d4dv/**", // specific to my uploadthing dashboard
+      },
+    ],
+  },
 };
 
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
   options: {
     remarkPlugins: [],
     rehypePlugins: [[rehypePrism, { ignoreMissing: true }]],
